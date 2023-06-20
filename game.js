73a3a615996042763764de753e168c9e1c09ee65
Palak <palakagrawal020@gmail.com>
2023-06-20 16:58:10 +0530

    Add a button to ask for the number of squares

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
+
 const container = document.querySelector('#container');
 
 function makeGrid(x) {
-    for(let i=0; i<16; i++){
+    for(let i=0; i<x; i++){
         let myRow = document.createElement('tr');
         myRow.id = "row" + i;
 
@@ -9,11 +10,11 @@ function makeGrid(x) {
 
         let row = document.getElementById("row" + i);
 
-        for(let j=0; j<16; j++){
+        for(let j=0; j<x; j++){
             let myCol = document.createElement('td');
 
             myCol.setAttribute('style', 'border: 1px solid #000; width:150px; height:150px; margin:0; padding:0;');
-            
+
             let cell = myCol.addEventListener('mouseover', function(e) {
                 e.target.style.background = 'black';
             });
@@ -23,5 +24,9 @@ function makeGrid(x) {
     }
 }
 
-makeGrid(16);
+makeGrid(16); 
 
+const btn = document.querySelector('#newGrid');
+btn.addEventListener('click', function (e) {
+    let size = prompt("Enter the number of squares to be added per side");
+});
